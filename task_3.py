cead423e859580f28f4cb60f569a69f4d7e06d92
Aleksandr <storeplase@gmail.com>
2026-03-15 20:06:49 +0400

    Исправление замечаний:  используем self и наследование методов

--- task_3.py
+++ task_3.py
@@ -26,9 +26,8 @@ class PointsForMeters:
 
 class TotalPoints(PointsForPlace, PointsForMeters):
     
-    @staticmethod
-    def get_total_points(meters, place):
-        total = PointsForPlace.get_points_for_place(place) + PointsForMeters.get_points_for_meters(meters)
+    def get_total_points(self,meters, place):
+        total = self.get_points_for_place(place) + self.get_points_for_meters(meters)
         return total
 
 points_for_place = PointsForPlace()
